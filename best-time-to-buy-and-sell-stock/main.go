4d21d6201e19d0a7d093a8d1fce8f9d794cc710d
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2023-01-05 11:47:09 +0900

    Solve best time to buy and sell stock

---
+++ best-time-to-buy-and-sell-stock/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func maxProfit(prices []int) int {
+	min := prices[0]
+	profit := 0
+	for i := 0; i < len(prices); i++ {
+		if prices[i] < min {
+			min = prices[i]
+		}
+		if prices[i]-min > profit {
+			profit = prices[i] - min
+		}
+	}
+	return profit
+}
+
+func main() {
+	fmt.Println(maxProfit([]int{7, 1, 5, 3, 6, 4}))
+	fmt.Println(maxProfit([]int{7, 6, 4, 3, 1}))
+}
