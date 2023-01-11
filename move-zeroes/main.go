2297e1584c066955e09ea0759ae08666283aaa12
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2023-01-11 22:36:04 +0900

    Optimize move zeroes

--- move-zeroes/main.go
+++ move-zeroes/main.go
@@ -3,14 +3,11 @@ package main
 import "fmt"
 
 func moveZeroes(nums []int) {
+	lastNonZeroFoundAt := 0
 	for i := 0; i < len(nums); i++ {
-		for j := 0; j < len(nums); j++ {
-			if j == len(nums)-1 {
-				continue
-			}
-			if nums[j] == 0 {
-				nums[j], nums[j+1] = nums[j+1], nums[j]
-			}
+		if nums[i] != 0 {
+			nums[lastNonZeroFoundAt], nums[i] = nums[i], nums[lastNonZeroFoundAt]
+			lastNonZeroFoundAt++
 		}
 	}
 }
