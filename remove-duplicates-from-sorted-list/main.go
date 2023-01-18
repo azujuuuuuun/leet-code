8de26d7a97c7a49884c370a504132f106cc60b2f
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2023-01-18 12:40:40 +0900

    Solve remove duplicates from sorted list

---
+++ remove-duplicates-from-sorted-list/main.go
@@ -0,0 +1,23 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+
+	n := head
+	for n.Next != nil {
+		if n.Next.Val == n.Val {
+			n.Next = n.Next.Next
+		} else {
+			n = n.Next
+		}
+	}
+
+	return head
+}
