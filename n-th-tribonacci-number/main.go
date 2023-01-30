f0849855d8b87dd2ec51dd469054e7ebc33bd819
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2023-01-30 12:50:39 +0900

    Change memo arg type

--- n-th-tribonacci-number/main.go
+++ n-th-tribonacci-number/main.go
@@ -2,13 +2,13 @@ package main
 
 import "fmt"
 
-func rec(n int, memo *map[int]int) int {
-	v, ok := (*memo)[n]
+func rec(n int, memo map[int]int) int {
+	v, ok := memo[n]
 	if ok {
 		return v
 	}
 	num := rec(n-1, memo) + rec(n-2, memo) + rec(n-3, memo)
-	(*memo)[n] = num
+	memo[n] = num
 	return num
 }
 
@@ -17,7 +17,7 @@ func tribonacci(n int) int {
 	memo[0] = 0
 	memo[1] = 1
 	memo[2] = 1
-	return rec(n, &memo)
+	return rec(n, memo)
 }
 
 func main() {
