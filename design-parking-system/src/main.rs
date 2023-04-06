1d3216d193b32b842ec01ee9871cc239f430c616
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2023-04-06 19:51:28 +0900

    Optipize design parking system

--- design-parking-system/src/main.rs
+++ design-parking-system/src/main.rs
@@ -1,33 +1,21 @@
 struct ParkingSystem {
-    big: i32,
-    medium: i32,
-    small: i32,
+    spaces: Vec<i32>,
 }
 
 impl ParkingSystem {
     fn new(big: i32, medium: i32, small: i32) -> Self {
-        ParkingSystem { big, medium, small }
+        ParkingSystem {
+            spaces: vec![big, medium, small],
+        }
     }
 
     fn add_car(&mut self, car_type: i32) -> bool {
-        if car_type == 1 {
-            if self.big == 0 {
-                return false;
-            }
-            self.big -= 1;
-            true
-        } else if car_type == 2 {
-            if self.medium == 0 {
-                return false;
-            }
-            self.medium -= 1;
+        let index = (car_type - 1) as usize;
+        if self.spaces[index] > 0 {
+            self.spaces[index] -= 1;
             true
         } else {
-            if self.small == 0 {
-                return false;
-            }
-            self.small -= 1;
-            true
+            false
         }
     }
 }
