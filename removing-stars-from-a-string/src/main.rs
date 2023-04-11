9adda21beef6ea213aa37be962745bc547abb374
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2023-04-11 12:42:46 +0900

    Optimize removing stars from a string

--- removing-stars-from-a-string/src/main.rs
+++ removing-stars-from-a-string/src/main.rs
@@ -2,25 +2,17 @@ struct Solution {}
 
 impl Solution {
     pub fn remove_stars(s: String) -> String {
-        let len = s.len();
-        let chars: Vec<_> = s.chars().collect();
-        let mut count = 0;
-        let mut stars_removed_chars = vec![];
+        let mut stack = vec![];
 
-        for i in 0..len {
-            let c = chars[len - 1 - i];
-            if c == '*' {
-                count += 1;
-            } else if count == 0 {
-                stars_removed_chars.push(c);
+        s.chars().for_each(|c| {
+            if c != '*' {
+                stack.push(c);
             } else {
-                count -= 1;
+                stack.pop();
             }
-        }
+        });
 
-        stars_removed_chars.reverse();
-
-        stars_removed_chars
+        stack
             .iter()
             .map(|c| c.to_string())
             .collect::<Vec<_>>()
