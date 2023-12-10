d3cad5bd568ba871fa2a12e02f24906be75595d1
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2023-12-10 16:50:32 +0900

    Solve transpose matrix

---
+++ transpose-matrix/main.ts
@@ -0,0 +1,35 @@
+function transpose(matrix: number[][]): number[][] {
+  const transposed: number[][] = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    const row: number[] = [];
+    for (let j = 0; j < matrix.length; j++) {
+      row.push(matrix[j][i]);
+    }
+    transposed.push(row);
+  }
+  return transposed;
+}
+
+console.log(
+  transpose([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ]),
+  [
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+  ]
+);
+console.log(
+  transpose([
+    [1, 2, 3],
+    [4, 5, 6],
+  ]),
+  [
+    [1, 4],
+    [2, 5],
+    [3, 6],
+  ]
+);
