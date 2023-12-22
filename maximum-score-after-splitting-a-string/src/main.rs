38b07352d197397d98142ba605090cbc9b51db4f
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2023-12-22 09:39:54 +0900

    Solve maximum score after splitting a string

--- maximum-score-after-splitting-a-string/src/main.rs
+++ maximum-score-after-splitting-a-string/src/main.rs
@@ -1,26 +1,24 @@
+use std::borrow::Borrow;
+
 struct Solution {}
 
 impl Solution {
     pub fn max_score(s: String) -> i32 {
         let mut max = 0;
-
         for i in 1..s.len() {
             let (left, right) = s.split_at(i);
-            let zeros = left.chars().filter(|c| *c == '0').count();
-            let ones = right.chars().filter(|c| *c == '1').count();
-            max = if (zeros + ones) as i32 > max {
-                (zeros + ones) as i32
-            } else {
-                max
+            let zeros = left.chars().filter(|c| c.eq('0'.borrow())).count();
+            let ones = right.chars().filter(|c| c.eq('1'.borrow())).count();
+            if zeros + ones > max {
+                max = zeros + ones;
             }
         }
-
-        max
+        max as i32
     }
 }
 
 fn main() {
-    println!("{}", Solution::max_score("011101".to_string()));
-    println!("{}", Solution::max_score("00111".to_string()));
-    println!("{}", Solution::max_score("1111".to_string()));
+    println!("{} {}", Solution::max_score("011101".to_string()), 5);
+    println!("{} {}", Solution::max_score("00111".to_string()), 5);
+    println!("{} {}", Solution::max_score("1111".to_string()), 3);
 }
