7d947308f1aa0fca3d807f2b77bd9dae64b7baf3
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2024-01-13 11:47:10 +0900

    Solve find common elements between two arrays by simple solution

--- find-common-elements-between-two-arrays/src/main.rs
+++ find-common-elements-between-two-arrays/src/main.rs
@@ -1,38 +1,25 @@
-use std::collections::HashMap;
-
 struct Solution {}
 
 impl Solution {
     pub fn find_intersection_values(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
-        let mut nums1_map = HashMap::new();
-        let mut nums2_map = HashMap::new();
-        nums1.iter().for_each(|n| {
-            if let Some(count) = nums1_map.get(n) {
-                nums1_map.insert(*n, count + 1);
-            } else {
-                nums1_map.insert(*n, 1);
-            }
-        });
-        nums2.iter().for_each(|n| {
-            if let Some(count) = nums2_map.get(n) {
-                nums2_map.insert(*n, count + 1);
-            } else {
-                nums2_map.insert(*n, 1);
-            }
-        });
-
         let mut nums1_count = 0;
         let mut nums2_count = 0;
-        nums1_map.iter().for_each(|(n, c)| {
-            if let Some(_) = nums2_map.get(n) {
-                nums1_count += c;
+        for i in 0..nums1.len() {
+            for j in 0..nums2.len() {
+                if nums1[i] == nums2[j] {
+                    nums1_count += 1;
+                    break;
+                }
             }
-        });
-        nums2_map.iter().for_each(|(n, c)| {
-            if let Some(_) = nums1_map.get(n) {
-                nums2_count += c;
+        }
+        for i in 0..nums2.len() {
+            for j in 0..nums1.len() {
+                if nums2[i] == nums1[j] {
+                    nums2_count += 1;
+                    break;
+                }
             }
-        });
+        }
         vec![nums1_count, nums2_count]
     }
 }
