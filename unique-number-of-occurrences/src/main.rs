9f2a26025c99dd2dbaaeeb8a27cf6a1ba255cc29
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2024-01-17 10:00:21 +0900

    Solve unique number of occurrence

--- unique-number-of-occurrences/src/main.rs
+++ unique-number-of-occurrences/src/main.rs
@@ -5,18 +5,17 @@ struct Solution {}
 impl Solution {
     pub fn unique_occurrences(arr: Vec<i32>) -> bool {
         let mut map = HashMap::new();
-
         arr.iter().for_each(|n| {
-            if map.contains_key(n) {
-                map.insert(n, map.get(n).unwrap() + 1);
+            if let Some(count) = map.get(n) {
+                map.insert(*n, *count + 1);
             } else {
-                map.insert(n, 1);
+                map.insert(*n, 1);
             }
         });
 
         let mut set = HashSet::new();
         map.values().for_each(|c| {
-            set.insert(c);
+            set.insert(*c);
         });
 
         map.len() == set.len()
@@ -24,10 +23,15 @@ impl Solution {
 }
 
 fn main() {
-    println!("{}", Solution::unique_occurrences(vec![1, 2, 2, 1, 1, 3]));
-    println!("{}", Solution::unique_occurrences(vec![1, 2]));
     println!(
-        "{}",
-        Solution::unique_occurrences(vec![-3, 0, 1, -3, 1, 1, 1, -3, 10, 0])
+        "{} {}",
+        Solution::unique_occurrences(vec![1, 2, 2, 1, 1, 3]),
+        true
+    );
+    println!("{} {}", Solution::unique_occurrences(vec![1, 2]), false);
+    println!(
+        "{} {}",
+        Solution::unique_occurrences(vec![-3, 0, 1, -3, 1, 1, 1, -3, 10, 0]),
+        true
     );
 }
