383c61684340265c8c7b33cc5d4836c103e7e065
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2024-01-17 23:12:03 +0900

    Solve sum of left leaves

---
+++ sum-of-left-leaves/main.ts
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ */
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function dfs(node: TreeNode | null, isLeft: boolean): number {
+  if (!node) {
+    return 0;
+  }
+  if (!node.left && !node.right) {
+    if (isLeft) {
+      return node.val;
+    } else {
+      return 0;
+    }
+  }
+  let sum = 0;
+  if (node.left) {
+    sum += dfs(node.left, true);
+  }
+  if (node.right) {
+    sum += dfs(node.right, false);
+  }
+  return sum;
+}
+
+function sumOfLeftLeaves(root: TreeNode | null): number {
+  return dfs(root, false);
+}
+
+console.log(
+  sumOfLeftLeaves(
+    new TreeNode(
+      3,
+      new TreeNode(9),
+      new TreeNode(20, new TreeNode(15), new TreeNode(7))
+    )
+  ),
+  24
+);
+console.log(sumOfLeftLeaves(new TreeNode(1)), 0);
