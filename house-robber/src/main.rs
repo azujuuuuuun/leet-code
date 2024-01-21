4d92b13b79d147599d04fd07b2243d6112716f71
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2024-01-21 20:38:11 +0900

    Optimized house robber

--- house-robber/src/main.rs
+++ house-robber/src/main.rs
@@ -1,30 +1,18 @@
+use std::cmp::max;
+
 struct Solution {}
 
 impl Solution {
     pub fn rob(nums: Vec<i32>) -> i32 {
-        let mut dp = vec![];
-        nums.iter().enumerate().for_each(|(i, n)| {
-            if i == 0 {
-                dp.push((0, *n));
-            } else if i == 1 {
-                dp.push((Self::max(dp[0].0, dp[0].1), dp[0].0 + *n));
-            } else {
-                dp.push((
-                    Self::max(dp[i - 1].0, dp[i - 1].1),
-                    Self::max(dp[i - 2].1, dp[i - 1].0) + *n,
-                ));
-            }
+        let mut not_robbed = 0;
+        let mut robbed = 0;
+        nums.iter().for_each(|n| {
+            let new_not_robbed = max(not_robbed, robbed);
+            let new_robbed = not_robbed + *n;
+            not_robbed = new_not_robbed;
+            robbed = new_robbed;
         });
-        let (left, right) = dp.last().unwrap();
-        Self::max(*left, *right)
-    }
-
-    fn max(num1: i32, num2: i32) -> i32 {
-        if num1 > num2 {
-            num1
-        } else {
-            num2
-        }
+        max(robbed, not_robbed)
     }
 }
 
